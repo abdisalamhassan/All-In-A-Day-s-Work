54e1b6a1e06a2f4a1547c9999da5dd316bde0a70
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-01-29 20:30:28 -0500

    add javascript

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+function sevenAM() {
+    var input_textarea = document.querySelector("#hour7am");
+    var output_div = document.querySelector("#comment7am");
+    var saveBtn = document.querySelector("#button7am");
+  
+    saveBtn.addEventListener("click", textContent);
+  
+    output_div.textContent = localStorage.getItem("content");
+    input_textarea.value = localStorage.getItem("content");
+  
+    function updateOutput() {
+      localStorage.setItem("content", textContent);
+  
+      
+    }
+  }
