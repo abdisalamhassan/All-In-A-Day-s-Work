4e5f363eb721fc054e263f0d0cc2f7b205ad2f02
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-01-30 08:02:35 -0500

    add ReadMe

---
+++ README.md
@@ -0,0 +1,20 @@
+## Description
+This is a simply daily planner app which allows the user to save events. It runs within the browser and uses the following technologies:
+-HTML
+-CSS
+-Javascript
+-jQuery
+
+## How it works
+The User would like a daily planner to help plan for a busy workday. The user has a set of timeblocks(7am-5pm) that they can enter tasks into. When the user enters a tasks they can hit the save button which holds that information into local storage. When the user refreshes the page the data will persist in the window.
+
+## Screenshots 
+
+
+
+
+
+
+## Contribution
+
+By: Abdi Hassan
--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-function sevenAM() {
-    var input_textarea = document.querySelector("#hour7am");
-    var output_div = document.querySelector("#comment7am");
-    var saveBtn = document.querySelector("#button7am");
-  
-    saveBtn.addEventListener("click", textContent);
-  
-    output_div.textContent = localStorage.getItem("content");
-    input_textarea.value = localStorage.getItem("content");
-  
-    function updateOutput() {
-      localStorage.setItem("content", textContent);
-  
-      
+let saveBtn = $(".saveBtn");
+
+
+function saveTasks () {
+    let currentTime = $(this).data("hour");
+    let rowHour = $(this).siblings(".hour").text();
+    let task = $(this).siblings(".textarea").val();
+
+    
+    if (task === "") {
+        localStorage.setItem(textarea, "");
     }
-  }
+    else {
+        localStorage.setItem(textarea, task);
+    }
+}
+
+saveBtn.on("click", saveTasks);
+
+
+
+description.each(function () {
+    let timeBlock = parseInt($(this).attr("id"));
+
+    if (timeBlock === currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+    }
+    else if (timeBlock < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+    }
+});
+
+
+description.each(function() {
+
+    for (let i = 0; i < localStorage.length; i++) {
+        let objectKey = localStorage.key(i);
+        let taskValue = localStorage.getItem(textarea);
+        let rowHour = $(this).siblings("").text();
+}
+
+})
